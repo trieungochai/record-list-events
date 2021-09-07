9ceeb53eaed64ccb711c18e8750a7520e24be109
Cai <trieungochai.dev@gmail.com>
2021-09-07 13:47:06 +0700

    Creating the Event List Component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Recorder from "../Recorder/Recorder";
+import Calendar from "../Calendar/Calendar";
 
 const App: React.FC = () => {
   return (
     <div>
       <Recorder />
+      <Calendar />
     </div>
   );
 };
